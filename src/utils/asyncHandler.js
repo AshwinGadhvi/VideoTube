93994c89ad30b4c57e01ee1170a16698fdf95069
AshwinGadhvi <ashwingadhvi397@gmail.com>
2025-05-25 22:54:42 +0530

    Rename The FileName.

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
-const asyncHander = (requestHandler)=>{
+const asyncHandler = (requestHandler)=>{
     (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
     }
 }
 
-export {asyncHander}
+export {asyncHandler}
 
 // const asyncHander = (fn)=> async(req,res,next)=>{
 //     try {
