07fbd06a8f2366c5dd296abbd45ab6bdd3b8cf36
AshwinGadhvi <ashwingadhvi397@gmail.com>
2025-05-27 00:41:04 +0530

    fixing sum bug.

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -15,7 +15,8 @@ const uploadOnCloudinary = async (localFilePath)=>{
         const response = await cloudinary.uploader.upload(localFilePath,{
             resource_type:"auto"
         })
-        console.log("file upload successfully : ",response.url);
+        // console.log("file upload successfully : ",response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {
         fs.unlink(localFilePath); //remove locally save temprory file if operation got failedr
